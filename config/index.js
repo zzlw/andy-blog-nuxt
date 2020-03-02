49632da87069370f52ac37e6af9eb8e162251896
zzlwte <zzlwte@gmail.com>
2020-03-02 10:56:16 +0800

    ok

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 const Config = {
-  baseUrl: process.env.NODE_ENV === 'production' ? 'https://47.115.125.250' : 'http://localhost:3000',
-  staticPath: '47.115.125.250'
+  baseUrl: process.env.NODE_ENV === 'production' ? 'http://47.115.125.250:3000' : 'http://47.115.125.250:3000',
+  staticPath: 'http://47.115.125.250:3000'
 }
-
 export default Config
